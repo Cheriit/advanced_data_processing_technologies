d6a2b13c8326c2db4e5bfa175345948f8db52bf1
Filip 'Cherit' Szóstak <f.szostak44@gmail.com>
2022-11-26 16:31:22 +0100

    4th laboratories

---
+++ ZTPD_4.sql
@@ -0,0 +1,76 @@
+-----
+--A--
+-----
+
+CREATE TABLE FIGURY (
+     ID number(1) PRIMARY KEY,
+     KSZTALT MDSYS.SDO_GEOMETRY
+);
+
+-----
+--B--
+-----
+
+INSERT INTO FIGURY
+VALUES (
+     1,
+     MDSYS.SDO_GEOMETRY (
+         2003,
+         NULL,
+         NULL,
+         MDSYS.SDO_ELEM_INFO_ARRAY(1, 1003, 4),
+         MDSYS.SDO_ORDINATE_ARRAY(5,7, 3,5, 5,3))
+);
+
+INSERT INTO FIGURY
+VALUES (
+     2,
+     MDSYS.SDO_GEOMETRY (
+         2003,
+         NULL,
+         NULL,
+         MDSYS.SDO_ELEM_INFO_ARRAY(1, 1003, 3),
+         MDSYS.SDO_ORDINATE_ARRAY(1,1, 5,5))
+);
+
+INSERT INTO FIGURY
+VALUES (
+     3,
+     MDSYS.SDO_GEOMETRY (
+         2006,
+         NULL,
+         NULL,
+         MDSYS.SDO_ELEM_INFO_ARRAY(1,4,2, 1,2,1, 5,2,2),
+         MDSYS.SDO_ORDINATE_ARRAY(3,2, 6,2, 7,3, 8,2, 7,1))
+);
+
+-----
+--C--
+-----
+INSERT INTO FIGURY
+VALUES (
+     4,
+     MDSYS.SDO_GEOMETRY (
+         2103,
+         NULL,
+         NULL,
+         MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1),
+         MDSYS.SDO_ORDINATE_ARRAY(1,1, 5,5, 5,1))
+);
+
+-----
+--D--
+-----
+SELECT ID, SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(KSZTALT, 0.01) AS VAL
+FROM FIGURY;
+
+-----
+--E--
+-----
+DELETE FROM FIGURY
+WHERE SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(KSZTALT, 0.01) NOT LIKE 'TRUE';
+
+-----
+--F--
+-----
+COMMIT;
